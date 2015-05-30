ea2f9b981357e5b1ec4d46469188f64f3977dba4
Simon Smith <me@simonsmith.io>
2015-05-30 10:47:39 +0100

    Allow array of string or regex ignores - fixes #2

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert    = require('assert');
 var fs        = require('fs');
-var postcss    = require('postcss');
+var postcss   = require('postcss');
 var classPrfx = require('..');
 
 function fixture(name) {
@@ -22,6 +22,22 @@ describe('postcss-class-prefix', function() {
               .use(classPrfx('prfx-', { ignore: /^is-/ }))
               .process(fixture('filter.css'));
     var expected = fixture('filter.expected.css');
+
+    assert.equal(output, expected);
+  });
+
+  it('allows an array of ignores as regex or string', function() {
+    var output = postcss()
+              .use(classPrfx('prfx-', {
+                ignore: [
+                  /^s-[a-z-]+/,
+                  'no-flexbox',
+                  'no-fontface',
+                  /^Box[a-zA-Z]/
+                  ]
+                }))
+              .process(fixture('ignore.css')).css;
+    var expected = fixture('ignore.expected.css');
 
     assert.equal(output, expected);
   });
